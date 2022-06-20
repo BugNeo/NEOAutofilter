9c1fccd109a27c9be222bccc99e3a2bd3c55e182
Gaurav <neodev268@gmail.com>
2022-06-20 21:19:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # MIT License
-# Copyright (c) 2022 Muhammed
+# Copyright (c) 2022 NEO
 import os, re
 search = re.compile(r'^.\d+$')
 def is_enabled(value, default):
@@ -12,8 +12,8 @@ def is_enabled(value, default):
 
 
 # Creator
-CREATOR_NAME = os.environ.get("CREATOR_NAME", "𝙼𝚄𝙷𝙰𝙼𝙼𝙴𝙳")
-CREATOR_USERNAME = os.environ.get("CREATOR_USERNAME", "PR0FESS0_99")
+CREATOR_NAME = os.environ.get("CREATOR_NAME", "NEO")
+CREATOR_USERNAME = os.environ.get("CREATOR_USERNAME", "NE069")
 
 # Account
 API_HASH = os.environ.get("API_HASH", "5b1d0992294a67cb54512a4fafeb0c88")
@@ -25,8 +25,8 @@ PICS = os.environ.get("PICS", "https://telegra.ph/file/034d53b5ed1d920ecab8b.jpg
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "LuciferMoringstar-Robot")
 DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://{Username}:{Passs}@cluster0.{clusterID}.mongodb.net/myFirstDatabase?retryWrites=true{iD}=majority")
 # Chats & Users
-ADMINS = os.environ.get("ADMINS", "2028425293 1637186875")
-SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Mo_Tech_Group")
+ADMINS = os.environ.get("ADMINS", "1975235651 1975235651")
+SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "RelaxroomChat1")
 AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "-1001685151224")
 CHANNELS = [int(ch) if search.search(ch) else ch for ch in os.environ.get("CHANNELS", "-1001784382279").split()]
 LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001590063851")
