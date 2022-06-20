b30087932edd531b8c5b4720bd485c16901d54d6
Gaurav <neodev268@gmail.com>
2022-06-20 21:49:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /NE0-Auto-Filter-Robot
 fi
-cd /EvaMaria
+cd /NE0-Auto-Filter-Robot
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
+echo "Starting NE0RoBot...."
+python3 main.py
