530e6d62eb127c94b93c862c892b32460cc0d8f6
Gaurav <neodev268@gmail.com>
2022-06-22 18:16:34 +0530

    Update connections.py

--- NEOAutofilter/modules/connections.py
+++ NEOAutofilter/modules/connections.py
@@ -1,7 +1,7 @@
 from pyrogram import filters, Client, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.connections_mdb import add_connection, all_connections, if_active, delete_connection
-from LuciferMoringstar_Robot import ADMINS
+from NEOAutofilter import ADMINS
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
